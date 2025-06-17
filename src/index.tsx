b7c6a8ccc26b8124244d023d52056ac87c02caa9
Ryuya Asada <ryuya0v0mac@gmail.com>
2025-06-17 22:54:18 +0900

    ファイル分割

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-import Header from './components/Header';
-
-export default function App() {
-    return (
-        <>
-            <Header />
-            <main>
-                <p>Main Content.</p>
-            </main>
-        </>
-    )
-};
+import App from './App';
+import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
