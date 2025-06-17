797905428236124a89568b1adcf9e43811cda299
Ryuya Asada <ryuya0v0mac@gmail.com>
2025-06-17 21:27:10 +0900

    Headerコンポーネントを作成

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import Header from './components/Header';
+
 export default function App() {
     return (
-        <>
-            <h1>Hello World.</h1>
-        </>
+        <Header />
     )
 };
 
